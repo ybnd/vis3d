917d72c3757095f48400d8135ac7489ecd791d77
Yury Bondarenko <bndyrv@gmail.com>
2018-10-23 15:05:42 +0200

    Initial commit

---
+++ imshow/imshow_tight_ortho.m
@@ -0,0 +1,53 @@
+function [handle_XY, handle_XZ, handle_YZ] = imshow_tight_ortho(image, slice, slice_method, slice_args, M, z, pad)
+% https://nl.mathworks.com/matlabcentral/answers/100366-how-can-i-remove-the-grey-borders-from-the-imshow-plot-in-matlab-7-4-r2007a
+    % pad: [bottom, top, left, right] -> dpos [left edge, bottom edge, width, height]
+
+    default_pad = [5,5,5,5];
+    
+    switch nargin
+        case 4
+            M = 100;
+            z = M;
+            pad = [0,0,0,0] + default_pad;
+        case 5
+            z = M;
+            pad = [0,0,0,0] + default_pad;
+        case 6
+            pad = [0 0 0 0] + default_pad;
+    end
+    
+    pad = pad + default_pad;
+    
+    dfpos = [0 0 pad(3)+pad(4) pad(1)+pad(2)];
+    dhpos = [pad(3) pad(1) 0 0];
+    
+    XY = slice_method(image,slice(3),slice_args);
+    XZ = slice_method(permute(image,[1,3,2]),slice(2),slice_args);
+    YZ = slice_method(permute(image,[3,2,1]),slice(1),slice_args);
+    
+    [Ny,Nx,Nz] = size(image);
+    X = Nx*M;
+    Y = Ny*M;
+    Z = Nz*M*z;
+    
+    ax_XY = subplot(2,2,1);
+    handle_XY = imshow(XY, 'InitialMagnification', M);
+    hold on
+    set(ax_XY, 'units', 'pixels')
+    ax_XZ = subplot(2,2,2);
+    handle_XZ = imshow(XZ, 'InitialMagnification', M);
+    hold on
+    daspect([1,z,1])
+    set(ax_XZ, 'units', 'pixels')
+    ax_YZ = subplot(2,2,3);
+    handle_YZ = imshow(YZ, 'InitialMagnification', M);
+    hold on
+    daspect([z,1,1])
+    set(ax_YZ, 'units', 'pixels')
+    
+    set(gcf, 'Position', [50, 50, X+Z, Y+Z] + dfpos);
+    set(ax_XY, 'Position', [0,Z+2,X,Y] + dhpos);
+    set(ax_XZ, 'Position', [X+2,Z+2,Z,Y] + dhpos);
+    set(ax_YZ, 'Position', [0,0,X,Z] + dhpos);  
+end
+
---
+++ slicestack.m
@@ -0,0 +1,59 @@
+function fig = slicestack(fig, C, slice_method, slice_args) 
+    switch nargin
+        case 2
+            slice_method = @normalize_slice;
+            slice_args = struct();
+        case 3
+            slice_args = struct();
+    end
+
+    if ~isempty(fig.UserData)
+        slice = fig.UserData;
+    else
+        slice = 1;
+        fig.UserData = 1;
+    end
+    
+    if ishandle(fig)
+        figure(fig);
+        set(fig, 'visible', 'on')
+    else
+        fig = figure;
+        set(fig, 'visible', 'on')        
+    end
+
+    if isempty(get(gca, 'Children'))
+        image = imshow_tight(slice_method(C,slice,slice_args), 150, [30,0,0,0]);
+            [~, ~, Nz] = size(C);
+        ui_text = uicontrol('style', 'text', ...
+            'Position', [5,3,45,20], ...
+            'String', sprintf('z(%d)',slice));
+        ap = get(gca, 'Position');
+        ui_slider = uicontrol('style', 'slider', ...
+            'Position', [55,5,ap(3)-58,20] ,...
+            'Value', slice, 'min', 1, 'max', Nz, ...
+            'SliderStep', [1/Nz, 1/Nz] ...
+            );
+        addlistener(ui_slider, 'Value', 'PostSet', @slider_callback);
+        
+        set(fig, 'WindowScrollWheelFcn', @scroll);
+    end
+
+    function slider_callback(~, eventdata)
+        new_slice = floor(get(eventdata.AffectedObject, 'Value'));
+        eventdata.AffectedObject.Parent.UserData = new_slice;
+        ui_text.String = sprintf('z(%d)',new_slice);
+        image.set('CData', slice_method(C,new_slice,slice_args));
+    end
+
+    function scroll(~, eventdata)
+        try
+            new_value = get(ui_slider, 'Value') - 1 * eventdata.VerticalScrollCount;
+            if new_value <= get(ui_slider, 'max') && new_value >= get(ui_slider, 'min')
+                set(ui_slider, 'Value', new_value);
+            end
+        catch
+            return
+        end
+    end
+end
