fc6efe90239f1ba7f27f8eb778f1dc4647c6a5c1
Yury Bondarenko <ybnd@tuta.io>
2019-10-12 17:48:51 +0200

    Clean up InteractiveMethod / InteractiveMethodSelector callbacks

--- ML/cube/InteractiveMethodSelector.m
+++ ML/cube/InteractiveMethodSelector.m
@@ -5,9 +5,10 @@ classdef InteractiveMethodSelector < dynamicprops
     properties
         name
         items
+        selected
     end
     
-    properties
+    properties(Hidden=true)        
         figure;
         callback = false;
         gui_handle = false;
@@ -22,12 +23,13 @@ classdef InteractiveMethodSelector < dynamicprops
             obj.items = items;
         end
         
-        function im = select(obj, item)
+        function selected = select(obj, item)
             % Sanity check: item in obj.item.field?
             if isfield(obj.items, item)
-                im = obj.items.(item);
+                selected = obj.items.(item);
+                obj.selected = selected;
                 if ishandle(obj.figure)
-                    obj.replace_controls(im)
+                    obj.replace_controls(selected)
                 end
                 obj.controls_callback() % todo: temporary, this is not a good idea if values for (source, event) are used!
             else
@@ -35,7 +37,7 @@ classdef InteractiveMethodSelector < dynamicprops
             end
         end
         
-        function gui_handle = build_gui(obj, figure, anchor, callback, controls_callback)
+        function gui_handle = build_gui(obj, figure, anchor, callback)
             global gui
             if isempty(gui)
                 interactive_methods;
@@ -49,28 +51,31 @@ classdef InteractiveMethodSelector < dynamicprops
             obj.gui_handle = gui_handle;
             
             obj.figure = figure;
-            obj.callback = callback;
-            obj.controls_callback = controls_callback;
+            obj.controls_callback = callback;
             obj.controls_anchor = anchor + [gui.selector_width + gui.gap, 0];
             
             % Show controls for first InteractiveMethod by default
             temp_items = fields(obj.items);
             obj.select(temp_items{1});
         end
+        
+        function out = do(obj, in)
+            % Pass to selected InteractiveMethod
+            out = obj.selected.do(in);
+        end
     end
     
     methods(Access=protected)
-        function replace_controls(obj, item)
+        function replace_controls(obj, selected)
             if ishandle(obj.controls)
                 delete(obj.controls);
             end
-            obj.controls = item.build_gui(obj.figure, obj.controls_anchor, obj.controls_callback);
-            set(obj.gui_handle, 'UserData', item);
+            obj.controls = selected.build_gui(obj.figure, obj.controls_anchor, obj.controls_callback);
+            set(obj.gui_handle, 'UserData', selected);
         end
         
-        function gui_select_callback(obj, source, event)
+        function gui_select_callback(obj, ~, event)
             obj.select(event.AffectedObject.String{event.AffectedObject.Value});   
-            obj.callback(source,event);
         end
     end
 end
