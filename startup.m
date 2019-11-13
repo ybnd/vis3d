53c46d5ead3d68dde40acc3455b936665c1bbf3a
Yury Bondarenko <ybnd@tuta.io>
2019-11-13 15:00:26 +0100

    Remove PLS Toolbox from path if installed (interferes with built-in rescale.m)

--- startup.m
+++ startup.m
@@ -1,2 +1,7 @@
 % Copy this file to your MATLAB path or add this to your own startup.m file
+
+% Add vis3d MATLAB code to path
 addpath(genpath('vis3d/ML'));
+
+% If applicable, remove the PLS Toolbox from path (interferes with the use of built-in function rescale.m, maybe others)
+rmpath([toolboxdir('') '\' ls([toolboxdir('') '\PLS*'])])
